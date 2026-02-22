cd30b76ae056239f168cd2e6745b5a12d01c4df1
Prathmesh Nakate <prathmeshnakate24@gmail.com>
2026-02-22 09:50:18 +0530

    Day 2: Added Product struct and vector

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include<iostream>
+#include<fstream>
+#include<string>
+#include<iomanip>
+#include<vector>
+
 using namespace std;
+
+struct Product
+{
+    int id;
+    string name;
+    string category;
+    int quantity;
+    double price;
+};
+
+vector<Product> products;
 
 int main()
 {
