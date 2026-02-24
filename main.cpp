94a5d7b89c051a84a515acc17832d09db983cdbe
Prathmesh Nakate <prathmeshnakate24@gmail.com>
2026-02-24 18:57:37 +0530

    Day 4 : Added save_Product() Function

--- main.cpp
+++ main.cpp
@@ -36,6 +36,19 @@ void load_Products()
     file.close();
 }
 
+void save_Products()
+{
+    ofstream file("inventory.txt");
+    for(const auto& p : products)
+    {
+        file << p.id <<endl;
+        file << p.name <<endl;
+        file << p.category <<endl;
+        file << p.quantity << " " << p.price <<endl;
+    }
+    file.close();
+}
+
 int main()
 {
     cout<<"Inventory management System"<<endl;
