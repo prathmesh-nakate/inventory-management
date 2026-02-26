c985011b6eea9905b7c869bf19c9bbabe65d93b0
Prathmesh Nakate <prathmeshnakate24@gmail.com>
2026-02-26 18:27:20 +0530

    Day 6: Added display_Products() function

--- main.cpp
+++ main.cpp
@@ -82,6 +82,39 @@ void add_Products()
     cout<<endl<<"Product Added Successfully !"<<endl;
 }
 
+void display_Products()
+{
+    if(products.empty())
+    {
+        cout<<"\n No Products in The System Yet !"<<endl;
+        return;
+    }
+    cout<<"\n ====================================================================================="<<endl;
+    cout<<"                             All products                                               "<<endl;
+    cout<<"\n ====================================================================================="<<endl;
+
+    cout<< left << setw(6) << "ID"
+                << setw(29) << "Name"
+                << setw(15) << "Category"
+                << setw(10) << "Quantity"
+                << setw(10) << "Price" <<endl;
+
+
+    cout<<"======================================================================================"<<endl;
+
+    for(const auto& p : products)
+    {
+        cout<< left << setw(6) << p.id
+                << setw(29) << p.name
+                << setw(15) << p.category
+                << setw(10) << p.quantity
+                << "$" << fixed << setprecision(2) << p.price <<endl;
+    }
+
+    cout<<"======================================================================================"<<endl;
+    cout<<"Total Products : "<<products.size() <<endl;
+} 
+
 int main()
 {
     cout<<"Inventory management System"<<endl;
